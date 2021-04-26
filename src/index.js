a8c53b77950711dee6f2f7f29adbc91d153d3942
VictorGAG <60749046+VictorGAG@users.noreply.github.com>
2021-04-26 15:37:53 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './app';
+import App from './App';
 
 ReactDOM.render(<React.StrictMode><App /></React.StrictMode>,document.getElementById('root'));
